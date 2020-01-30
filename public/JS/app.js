f95a05e09b83ab10403dcf2927e83d519c704cdf
Baptiste Bidaux <baptistelise@orange.fr>
2020-01-30 18:59:05 +0100

    Background gradient + modifications progress bar

--- public/JS/app.js
+++ public/JS/app.js
@@ -17,6 +17,8 @@ $(".accordion").click(function() {
     } else {
         $(this).find('img[src*="pictures/gifs/arrow-up.gif"]').css('display', 'inline-block');
     }
+
+    inProgress();
 });
 
 // Gestion des liens actifs de la Navbar
@@ -76,11 +78,11 @@ $(document).ready(function() {
 })
 
 // Gestion des cadres "front" et "back" pour les appareils tactiles
-$('.flip-card-front').bind('touchstart', function(event) {
+$('.flip-card-front').bind('click', function(event) {
     event.preventDefault();
     $(this).closest('.flip-card-inner').toggleClass('back-is-visible'); 
 })
-$('.flip-card-back ul li').bind('touchstart', function(event) {
+$('.flip-card-back ul li').bind('click', function(event) {
     event.preventDefault();
     $(this).closest('.flip-card-inner').toggleClass('back-is-visible'); 
 })
@@ -131,64 +133,3 @@ lottie.loadAnimation({
     loop: true,
     autoplay: true,
 });
-
-// Gestion du background animé
-var colors = new Array(
-    // [0,0,128],
-    // [0,0,255],
-    // [1,31,85],
-    // [0,152,255],
-    // [32,146,223],
-    // [65,87,128]
-    [1, 31, 85],
-    [17, 43, 90],
-    [64, 79, 106],
-    [1, 27, 74],
-    [33, 55, 96],
-    [48, 67, 101]
-);
-  
-var step = 0;
-
-var colorIndices = [0,1,2,3];
-  
-var gradientSpeed = 0.002;
-  
-function updateGradient() {
-    
-    if ( $===undefined ) return;
-    
-    var c0_0 = colors[colorIndices[0]];
-    var c0_1 = colors[colorIndices[1]];
-    var c1_0 = colors[colorIndices[2]];
-    var c1_1 = colors[colorIndices[3]];
-    
-    var istep = 1 - step;
-    var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-    var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-    var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-    var color1 = "rgb("+r1+","+g1+","+b1+")";
-    
-    var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-    var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-    var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-    var color2 = "rgb("+r2+","+g2+","+b2+")";
-  
-    $('body').css({
-        background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-        background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"
-    });
-    
-    step += gradientSpeed;
-
-    if ( step >= 1 )
-    {
-        step %= 1;
-        colorIndices[0] = colorIndices[1];
-        colorIndices[2] = colorIndices[3];
-        colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-        colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-    }
-  }
-  
-  setInterval(updateGradient,10);
