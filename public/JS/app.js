1557fbf31621902ee3d98517a96c1f248b99e52f
Baptiste Bidaux <baptistelise@orange.fr>
2020-01-30 20:02:30 +0100

    Fix Bug Flip-card

--- public/JS/app.js
+++ public/JS/app.js
@@ -78,11 +78,11 @@ $(document).ready(function() {
 })
 
 // Gestion des cadres "front" et "back" pour les appareils tactiles
-$('.flip-card-front').bind('click', function(event) {
+$('.flip-card-front').bind('touchstart', function(event) {
     event.preventDefault();
     $(this).closest('.flip-card-inner').toggleClass('back-is-visible'); 
 })
-$('.flip-card-back ul li').bind('click', function(event) {
+$('.flip-card-back ul li').bind('touchstart', function(event) {
     event.preventDefault();
     $(this).closest('.flip-card-inner').toggleClass('back-is-visible'); 
 })
